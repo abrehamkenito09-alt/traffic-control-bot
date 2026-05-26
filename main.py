24bcd521d884db72c4d66257c52fb4cbeb816d30
Abreham Kenito <abrehamkenito09@gmail.com>
2026-05-26 16:01:07 +0300

    Refactor main.py with comments for clarity
    Added comments to clarify bot functionality and token retrieval.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
+# ቦቱ መልዕክቶችን እንዲያነብ ይፈቅዳል
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -11,5 +12,5 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'ቦቱ ኦንላይን ሆኗል: {bot.user}')
 
-# የራስዎን ኮዶች እዚህ መጻፍ ይችላሉ
+# የቦቱን ቶከን ከRailway Variables ውስጥ ይወስዳል
 bot.run(os.environ['DISCORD_TOKEN'])
