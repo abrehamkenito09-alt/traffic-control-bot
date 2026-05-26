6c4ae969de2d6ac8bae189a8625d60de30f87312
Abreham Kenito <abrehamkenito09@gmail.com>
2026-05-26 15:46:20 +0300

    Initialize Discord bot with commands and intents

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'ቦቱ ኦንላይን ሆኗል: {bot.user}')
+
+# የራስዎን ኮዶች እዚህ መጻፍ ይችላሉ
+bot.run(os.environ['DISCORD_TOKEN'])
